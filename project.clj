01019dae4d6bc118289bdd914da66c485fdffa3f
Osamu Nagano <onagano.g@gmail.com>
2011-08-25 21:22:49 +0900

    Server launch capability

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
-(defproject mob4clojure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject mob4clojure "0.1.0-prerelease"
+  :description "An application of MOB (MT4-ODBC Bridge) for Clojure programming language"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [com.h2database/h2 "1.3.157"]
+                 [joda-time/joda-time "1.6.2"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]]
+  :main mob4clojure.core)
--- src/mob4clojure/core.clj
+++ src/mob4clojure/core.clj
@@ -1,1 +1,21 @@
-(ns mob4clojure.core)
+(ns mob4clojure.core
+  (:gen-class))
+
+(def  environment (atom nil))
+
+(defn- get-environment-properties
+  []
+  (let [prop (doto (java.util.Properties.)
+               (.load (.getResourceAsStream (class "") "/mob4clojure.properties")))
+        keys (enumeration-seq (.keys prop))]
+    (apply hash-map
+           (apply concat
+                  (for [k keys] [(keyword k) (.get prop k)])))))
+
+(defn -main [& args]
+  (swap! environment (fn [_] (get-environment-properties)))
+  (.println *err* (format "mob4clojure.env.name: %s"
+                          (:mob4clojure.env.name @environment)))
+  ;; For server arguments:
+  ;; http://www.h2database.com/html/tutorial.html#using_server
+  (org.h2.tools.Server/main args))
